ea6b74f5bb3e949d65dd650965571d6cbfacd9b0
Gonçalo Leite <1241608@isep.ipp.pt>
2024-12-19 20:40:40 +0000

    CRUD/MEMÓRIA - RESERVAS E EMPRÉSTIMOS

--- Emprestimo.java
+++ Emprestimo.java
@@ -10,4 +10,78 @@ public class Emprestimo extends Transacao{
         this.dataPrevistaDevolucao = dataPrevistaDevolucao;
         this.dataEfetivaDevolucao = null;
     }
+
+    public Emprestimo(String numero, Utente utente, ArrayList<Livro> livros, String dataInicio, String dataPrevistaDevolucao, String dataEfetivaDevolucao) {
+        super(numero, utente, livros, dataInicio);
+        this.dataPrevistaDevolucao = dataPrevistaDevolucao;
+        this.dataEfetivaDevolucao = dataEfetivaDevolucao;
+    }
+
+    public String getDataPrevistaDevolucao() {
+        return dataPrevistaDevolucao;
+    }
+    public void setDataPrevistaDevolucao(String dataPrevistaDevolucao) {
+        this.dataPrevistaDevolucao = dataPrevistaDevolucao;
+    }
+    public String getDataEfetivaDevolucao() {
+        return dataEfetivaDevolucao;
+    }
+    public void setDataEfetivaDevolucao(String dataEfetivaDevolucao) {
+        this.dataEfetivaDevolucao = dataEfetivaDevolucao;
+    }
+
+    @Override
+    public String toString() {
+        ArrayList<String> isbnLivros = new ArrayList<>();
+        for (Livro livro : getLivros()) {
+            isbnLivros.add(livro.getISBN());
+        }
+        return "[" +
+                "Número: " + getNumero() +
+                "; NIF do Utente: " + getUtente().getNif() +
+                "; ISBN dos Livros: " + isbnLivros.toString().replace("[", "").replace("]", "") +
+                "; Data de Inicio: " + getDataInicio() +
+                "; Data Prevista Devolução: " + getDataPrevistaDevolucao() +
+                "; Data Efetiva Devolução: " + getDataEfetivaDevolucao() +
+                ']';
+    }
+
+    public String toFileString(){
+        ArrayList<String> isbnLivros = new ArrayList<>();
+        for (Livro livro : getLivros()) {
+            isbnLivros.add(livro.getISBN());
+        }
+        return getNumero() +
+                "|" + getUtente().getNif() +
+                "|" + isbnLivros.toString().replace("[", "").replace("]", "") +
+                "|" + getDataInicio() +
+                "|" + getDataPrevistaDevolucao() +
+                "|" + getDataEfetivaDevolucao();
+    }
+
+    public static Emprestimo fromString(String dados) {
+        String[] partes = dados.split("\\|");
+        String numero = partes[0];
+        String nif = partes[1];
+        Utente utente = SistemaGestaoBiblioteca.procurarUtentePorNIF(nif);
+        if (utente == null) {
+            throw new IllegalArgumentException("Utente com NIF " + nif + " não encontrado.");
+        }
+        ArrayList<Livro> livros = new ArrayList<>();
+        for (String isbn : partes[2].split(",")) {
+            Livro livro = SistemaGestaoBiblioteca.procurarLivroPorISBN(isbn.trim());
+            if (livro == null) {
+                throw new IllegalArgumentException("Livro com ISBN " + isbn + " não encontrado.");
+            }
+            livros.add(livro);
+        }
+        String dataInicio = partes[3];
+        String dataPrevistaDevolucao = partes[4];
+        String dataEfetivaDevolucao = partes[5];
+        if (dataEfetivaDevolucao == null) {
+            return new Emprestimo(numero, utente, livros, dataInicio, dataPrevistaDevolucao);
+        }else {
+            return new Emprestimo(numero, utente, livros, dataInicio, dataPrevistaDevolucao, dataEfetivaDevolucao);
+        }
+    }
 }
