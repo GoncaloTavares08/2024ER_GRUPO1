ded7e90d404d9975de9400458a249737382c7f30
João Rodrigues <1241613@isep.ipp.pt>
2025-01-11 16:30:39 +0000

    Total Empréstimos por Data

--- menus/emprestimos/MenuEmprestimos.java
+++ menus/emprestimos/MenuEmprestimos.java
@@ -6,8 +6,9 @@ import modelos.Biblioteca;
 public class MenuEmprestimos extends Menu {
     public MenuEmprestimos(Biblioteca biblioteca, String name) {
         super(biblioteca, name);
-        this.menus.add(new MenuListarEmprestimos(biblioteca, "Listar Emprestimos"));
-        this.menus.add(new MenuAdicionarEmprestimo(biblioteca, "Adicionar Emprestimo"));
-        this.menus.add(new MenuEditarEmprestimo(biblioteca, "Editar Emprestimo"));
+        this.menus.add(new MenuListarEmprestimos(biblioteca, "Listar Empréstimos"));
+        this.menus.add(new MenuTotalEmprestimosPorData(biblioteca, "Total de Empréstimos por Data"));
+        this.menus.add(new MenuAdicionarEmprestimo(biblioteca, "Adicionar Empréstimo"));
+        this.menus.add(new MenuEditarEmprestimo(biblioteca, "Editar Empréstimo"));
     }
 }
